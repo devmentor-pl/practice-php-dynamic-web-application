48094d168919832c5f487ffb97ed0c183e53e656
Cendlak <mateusz.cendlak@e-file.pl>
2025-01-22 00:28:53 +0100

    05 - sql injection

--- 05/index.php
+++ 05/index.php
@@ -1,1 +1,17 @@
 <?php
+
+$dsn = "mysql:host=localhost;port=3307;dbname=laracast;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$id = $_GET['id'];
+
+$statement = $pdo->query("select * from users where id = {$id}");
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$url = "http://localhost/sql-injection?id=1 OR 1=1";
+$url2 = "http://localhost/sql-injection?id=1; drop table users";
+
+//poprawnie:
+$statement = $pdo->query("select * from users where id = :id");
+$statement->execute([':id' => $id]);
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
